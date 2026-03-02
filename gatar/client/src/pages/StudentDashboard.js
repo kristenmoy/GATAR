dd3bc6885f641bab5d6d3f421450720729e28772
tramont-a <tramontbianca@gmail.com>
2026-03-02 15:32:34 -0500

    added chatbotify as frontend for our llm

--- gatar/client/src/pages/StudentDashboard.js
+++ gatar/client/src/pages/StudentDashboard.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ChatBot from "react-chatbotify";
 
 
 function StudentDashboard() {
+  // chatbot elements
+  const settings = {
+    general: {embedded:true, primaryColor:"#BD4F00", secondaryColor:"#150FA9"},
+    header: {title:"CIS4904"}
+  }
+  const flow = {
+    start: {
+        message: "Hi.",
+        path: "end_loop"
+    },
+    end_loop: {
+        message: "Connect LLM to this later.",
+        path: "end_loop"
+    }
+  }
   return (
-    <div>
-      <h1>Welcome to the Student Dashboard</h1>
-      <Link to="/">
-        <button>I wanna go home</button>
-      </Link>
+    <div className="dashboard-background">
+      <div className="center-screen">
+        <ChatBot settings={settings} flow={flow}/>
+      </div>
     </div>
   );
 }
