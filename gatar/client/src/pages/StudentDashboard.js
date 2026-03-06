a71e89334f1fd1b6fbe3604e8c33668c90c651cb
tramont-a <tramontbianca@gmail.com>
2026-03-06 14:05:44 -0500

    fixed window size for chatbot, added buttons to change courses (nonfunctional), removed scrolling

--- gatar/client/src/pages/StudentDashboard.js
+++ gatar/client/src/pages/StudentDashboard.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ChatBot from "react-chatbotify";
+import ChatBot, { ChatBotProvider } from "react-chatbotify";
+import { useState } from 'react';
 
+/* Current issues:
+1. figure out way to make the course change palpable
+   - restart chatbot UI, updateSettings to correct chatbot?
+   - switch chatbot/page entirely?
+2. link chatbot to UI [PRIORITY]
+   - talk to backend about that
+3. access student courses to reflect that in chatbot options
+   - wireframe: showed options already grabbed from student info + add a class opt.
+*/
 
 function StudentDashboard() {
+  // vars
+  const [course, changeCourse] = useState("CIS4904"); // change to student default
+  // functions
+  const handleClick = (code) => {
+    changeCourse(code);
+  }
   // chatbot elements
   const settings = {
     general: {embedded:true, primaryColor:"#BD4F00", secondaryColor:"#150FA9"},
-    header: {title:"CIS4904"}
+    header: {title:`${course}`}
   }
   const flow = {
     start: {
-        message: "Hi.",
+        message: `Welcome to ${course}. How can I help you today?`,
         path: "end_loop"
     },
     end_loop: {
@@ -22,7 +38,21 @@ function StudentDashboard() {
   return (
     <div className="dashboard-background">
       <div className="center-screen">
-        <ChatBot settings={settings} flow={flow}/>
+        <div className="left-side">
+          <div className="side-wrapper">
+            <h3>Class list:</h3>
+            <button type="button"
+            onClick={() => handleClick("mewo")}>mewo</button>
+            <button>test</button>
+            <button>class1</button>
+            <button>class2</button>
+          </div>
+        </div>
+        <div className="right-side">
+          <ChatBotProvider>
+            <ChatBot settings={settings} flow={flow}/>
+          </ChatBotProvider>
+        </div>
       </div>
     </div>
   );
